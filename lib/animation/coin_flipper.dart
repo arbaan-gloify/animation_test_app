6ef3ac3f662a903cb9c96b908a6bc5173da15437
Arbaan Quareshi S <arbaan.cumin@gmail.com>
2023-02-16 15:55:26 +0530

    added speech and route

---
+++ lib/animation/coin_flipper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CoinFLppee extends StatefulWidget {
+  const CoinFLppee({super.key});
+
+  @override
+  State<CoinFLppee> createState() => _CoinFLppeeState();
+}
+
+class _CoinFLppeeState extends State<CoinFLppee> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
